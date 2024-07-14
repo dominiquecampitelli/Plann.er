7b94c53415ecc092b51f64ac2539b516695a774a
Dominique Campitelli <dominiquecamp@outlook.com>
2024-07-14 16:16:58 -0300

    Date picker with date-fns

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -101,8 +101,8 @@ export function CreateTripPage() {
         </div>
 
         <p className="text-sm text-zinc-500">
-          Ao planejar sua viagem pela plann.er você automaticamente concorda com
-          nossos{" "}
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
           <a className="text-zinc-300 underline" href="#">
             termos de uso
           </a>{" "}
